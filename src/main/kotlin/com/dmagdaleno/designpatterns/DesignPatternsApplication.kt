b76e26a8d0dcaeaa3f100e85c69fd76f5e26f5ee
dmagdaleno <diegommagdaleno@gmail.com>
2019-03-15 10:23:27 -0300

    conversao do projeto para spring boot

---
+++ src/main/kotlin/com/dmagdaleno/designpatterns/DesignPatternsApplication.kt
@@ -0,0 +1,11 @@
+package com.dmagdaleno.designpatterns
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesignPatternsApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesignPatternsApplication>(*args)
+}
---
+++ src/test/kotlin/com/dmagdaleno/designpatterns/DesignPatternsApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.dmagdaleno.designpatterns
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DesignPatternsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
