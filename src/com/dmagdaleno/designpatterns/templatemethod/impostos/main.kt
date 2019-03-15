9b445489410848c7addc59b06073dc4f01cca354
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-03-14 22:46:40 -0300

    testa imposto IHIT

--- src/com/dmagdaleno/designpatterns/templatemethod/impostos/main.kt
+++ src/com/dmagdaleno/designpatterns/templatemethod/impostos/main.kt
@@ -3,11 +3,11 @@ package com.dmagdaleno.designpatterns.templatemethod.impostos
 fun main(){
 
     val itens = mutableListOf(
-        Item("Caneta", 99.0),
-        Item("Caneta", 99.0),
-        Item("Caneta", 99.0),
-        Item("Caneta", 95.0),
-        Item("Lapis", 299.0)
+        Item("Caneta", 100.0),
+        Item("Caneta", 100.0),
+        Item("Caneta", 100.0),
+        Item("Caneta", 100.0),
+        Item("Lapis", 101.0)
     )
 
     val orcamento = Orcamento(itens)
@@ -15,6 +15,22 @@ fun main(){
     val ikcv = IKCV()
     val icpp = ICPP()
 
-    println("Imposto: ${ikcv.calcula(orcamento)}")
-    println("Imposto: ${icpp.calcula(orcamento)}")
+    println("Imposto IKCV: ${ikcv.calcula(orcamento)}")
+    println("Imposto ICPP: ${icpp.calcula(orcamento)}")
+
+    val itensSemRepeticao = mutableListOf(
+        Item("Caneta1", 100.0),
+        Item("Caneta2", 100.0),
+        Item("Caneta3", 100.0),
+        Item("Caneta4", 100.0),
+        Item("Lapis", 101.0)
+    )
+
+    val orcamentoSemRepeticao = Orcamento(itensSemRepeticao)
+
+    val ihit = IHIT()
+
+    println("Imposto IHIT: ${ihit.calcula(orcamento)}")
+    println("Imposto IHIT: ${ihit.calcula(orcamentoSemRepeticao)}")
+
 }
