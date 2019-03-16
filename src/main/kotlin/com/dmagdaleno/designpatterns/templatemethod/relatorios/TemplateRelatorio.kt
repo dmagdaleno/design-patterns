15d0eab018b3cc9c77be2a0eda02c44d2c844106
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-03-15 22:48:07 -0300

    modelo abstrato de relatorio

--- src/main/kotlin/com/dmagdaleno/designpatterns/templatemethod/relatorios/TemplateRelatorio.kt
+++ src/main/kotlin/com/dmagdaleno/designpatterns/templatemethod/relatorios/TemplateRelatorio.kt
@@ -1,4 +1,14 @@
 package com.dmagdaleno.designpatterns.templatemethod.relatorios
 
-interface TemplateRelatorio {
+abstract class TemplateRelatorio {
+
+    fun imprime(banco: Banco, conta: Conta) =
+        "${geraCabecalho(banco)}\n\n${geraConteudo(conta)}\n\n${geraRodape(banco)}"
+
+    protected abstract fun geraCabecalho(banco: Banco): String
+
+    protected abstract fun geraConteudo(conta: Conta): String
+
+    protected abstract fun geraRodape(banco: Banco): String
+
 }
