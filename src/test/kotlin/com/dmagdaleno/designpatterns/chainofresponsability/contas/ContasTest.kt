8d6b72ad9101691b6e95e2414004a952f9a07e75
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-03-15 21:04:30 -0300

    testa respostas de acordo com o formato

--- src/test/kotlin/com/dmagdaleno/designpatterns/chainofresponsability/contas/ContasTest.kt
+++ src/test/kotlin/com/dmagdaleno/designpatterns/chainofresponsability/contas/ContasTest.kt
@@ -1,4 +1,61 @@
 package com.dmagdaleno.designpatterns.chainofresponsability.contas
 
+import org.assertj.core.api.Assertions
+import org.junit.Before
+import org.junit.Test
+
 class ContasTest {
+
+    val primeira = RespostaXML()
+    val segunda = RespostaCSV()
+    val tereceira = RespostaPorcento()
+
+    @Before
+    fun setup() {
+        primeira.setProximo(segunda)
+        segunda.setProximo(tereceira)
+        tereceira.setProximo(RespostaPadrao())
+    }
+
+    @Test
+    fun deveResponderEmXML() {
+        // dado que...
+        val conta = Conta("João da Silva", 1500.0)
+        val requisicao = Requisicao(Formato.XML)
+
+        // quando
+        val resposta = primeira.responde(requisicao, conta)
+
+        // então...
+        val respostaEsperada = "<conta><titular>João da Silva</titular><saldo>1500.0</saldo></conta>"
+        Assertions.assertThat(resposta).isEqualTo(respostaEsperada)
+    }
+
+    @Test
+    fun deveResponderEmCSV() {
+        // dado que...
+        val conta = Conta("João da Silva", 1500.0)
+        val requisicao = Requisicao(Formato.CSV)
+
+        // quando
+        val resposta = primeira.responde(requisicao, conta)
+
+        // então...
+        val respostaEsperada = "João da Silva,1500.0"
+        Assertions.assertThat(resposta).isEqualTo(respostaEsperada)
+    }
+
+    @Test
+    fun deveResponderEmPorcento() {
+        // dado que...
+        val conta = Conta("João da Silva", 1500.0)
+        val requisicao = Requisicao(Formato.PORCENTO)
+
+        // quando
+        val resposta = primeira.responde(requisicao, conta)
+
+        // então...
+        val respostaEsperada = "João da Silva%1500.0"
+        Assertions.assertThat(resposta).isEqualTo(respostaEsperada)
+    }
 }
