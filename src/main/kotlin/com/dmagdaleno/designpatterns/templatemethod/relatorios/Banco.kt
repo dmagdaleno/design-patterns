244f0134d9448f9add831713d0725d4aedb0e08d
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-03-15 22:47:33 -0300

    classes de dados para geração de relatorios

---
+++ src/main/kotlin/com/dmagdaleno/designpatterns/templatemethod/relatorios/Banco.kt
@@ -0,0 +1,7 @@
+package com.dmagdaleno.designpatterns.templatemethod.relatorios
+
+data class Banco(
+    val nome: String,
+    val endereco: String,
+    val telefone: String,
+    val email: String)
---
+++ src/main/kotlin/com/dmagdaleno/designpatterns/templatemethod/relatorios/Conta.kt
@@ -0,0 +1,8 @@
+package com.dmagdaleno.designpatterns.templatemethod.relatorios
+
+data class Conta(
+    val id: Int,
+    val idAgencia: Int,
+    val titular: String,
+    val saldo: Double
+)
