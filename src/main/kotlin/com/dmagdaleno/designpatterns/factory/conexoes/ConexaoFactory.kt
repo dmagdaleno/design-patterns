52e256260a55767392a67b964183bfa30a11fc7f
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-03-17 19:40:29 -0300

    utiliza conexao ficticia

--- src/main/kotlin/com/dmagdaleno/designpatterns/factory/conexoes/ConexaoFactory.kt
+++ src/main/kotlin/com/dmagdaleno/designpatterns/factory/conexoes/ConexaoFactory.kt
@@ -1,24 +1,8 @@
 package com.dmagdaleno.designpatterns.factory.conexoes
 
-import java.sql.Connection
-import java.sql.DriverManager
-import java.sql.SQLException
-
-class ConnectionFactory {
+class ConexaoFactory {
 
     companion object {
-        fun getConnection(): Connection? {
-            return try {
-
-                DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/banco",
-                    "usuario",
-                    "senha")
-
-            } catch (e: SQLException) {
-                println("nenhum driver encontrado")
-                null
-            }
-        }
+        fun getConnection() = Conexao("jdbc:mysql://localhost:3306/banco", "usuario", "senha")
     }
 }
