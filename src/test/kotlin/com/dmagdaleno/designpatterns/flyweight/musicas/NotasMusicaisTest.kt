13cf603cacd4d2cd603b81d1924d55d956d6b491
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-03-18 21:15:59 -0300

    verificando se melhorias surtiram efeito

--- src/test/kotlin/com/dmagdaleno/designpatterns/flyweight/musicas/NotasMusicaisTest.kt
+++ src/test/kotlin/com/dmagdaleno/designpatterns/flyweight/musicas/NotasMusicaisTest.kt
@@ -11,6 +11,7 @@ class NotasMusicaisTest {
         val notasMusicais2 = NotasMusicais()
 
         assertThat(notasMusicais1).isNotSameAs(notasMusicais2)
+
         assertThat(notasMusicais1.pega("Do")).isSameAs(notasMusicais2.pega("Do"))
         assertThat(notasMusicais1.pega("Re")).isSameAs(notasMusicais2.pega("Re"))
         assertThat(notasMusicais1.pega("Mi")).isSameAs(notasMusicais2.pega("Mi"))
@@ -18,5 +19,7 @@ class NotasMusicaisTest {
         assertThat(notasMusicais1.pega("Sol")).isSameAs(notasMusicais2.pega("Sol"))
         assertThat(notasMusicais1.pega("La")).isSameAs(notasMusicais2.pega("La"))
         assertThat(notasMusicais1.pega("Si")).isSameAs(notasMusicais2.pega("Si"))
+
+        assertThat(NotasMusicais.counter).isEqualTo(7)
     }
 }
