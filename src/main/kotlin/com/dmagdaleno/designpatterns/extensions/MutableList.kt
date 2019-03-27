905acb2cf1d9f3ab86cd7a1255d7a86fbdaba49b
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-03-26 22:13:05 -0300

    refatora função para remover ultimo item da lista

--- src/main/kotlin/com/dmagdaleno/designpatterns/extensions/MutableList.kt
+++ src/main/kotlin/com/dmagdaleno/designpatterns/extensions/MutableList.kt
@@ -1,6 +1,5 @@
 package com.dmagdaleno.designpatterns.extensions
 
 fun <E> MutableList<E>.removeLast() {
-    val lastIndex = this.lastIndex
-    this.removeAt(lastIndex)
+    this.removeAt(this.lastIndex)
 }
