1bfeb0f86663a76608bcf4c8478d4be0cf1da05c
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-04-08 22:49:03 -0300

    retorna operador de soma

--- src/main/kotlin/com/dmagdaleno/designpatterns/visitor/impressora/Soma.kt
+++ src/main/kotlin/com/dmagdaleno/designpatterns/visitor/impressora/Soma.kt
@@ -4,4 +4,6 @@ class Soma(expressaoEsquerda: Expressao, expressaoDireita: Expressao)
     : Operacao(expressaoEsquerda, expressaoDireita) {
 
     override fun avalia(valor1: Int, valor2: Int) = valor1 + valor2
+
+    override fun operador() = "+"
 }
