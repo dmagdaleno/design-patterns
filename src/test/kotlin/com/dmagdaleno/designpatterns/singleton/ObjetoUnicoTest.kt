efaa63f3990339d2743d03e92915cabb2f5cdfd9
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-04-21 14:53:43 -0300

    testa padrão singleton

---
+++ src/test/kotlin/com/dmagdaleno/designpatterns/singleton/ObjetoUnicoTest.kt
@@ -0,0 +1,28 @@
+package com.dmagdaleno.designpatterns.singleton
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.FixMethodOrder
+import org.junit.Test
+import org.junit.runners.MethodSorters
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+class ObjetoUnicoTest {
+
+    @Test
+    fun criaInstanciaDeFormaLazy() {
+        val chamada = ObjetoUnico.metodoEstaticoQueNaoPrecisaDeInstancia()
+        
+        assertThat(chamada).isEqualTo("chamou metodo")
+        assertThat(ObjetoUnico.qtdInstancias).isEqualTo(0)
+    }
+
+    @Test
+    fun deveGerarApenasUmaInstancia() {
+        val ref1 = ObjetoUnico.getInstancia()
+        val ref2 = ObjetoUnico.getInstancia()
+
+        assertThat(ref1).isSameAs(ref2)
+        assertThat(ref1.atributo).isSameAs(ref2.atributo)
+        assertThat(ObjetoUnico.qtdInstancias).isEqualTo(1)
+    }
+}
