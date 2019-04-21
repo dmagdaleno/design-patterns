a040e411c36ad8aa331ba02925bcefbde63d5c71
Diego Magdaleno <diegommagdaleno@gmail.com>
2019-04-21 11:29:20 -0300

    formatação de código

--- src/main/kotlin/com/dmagdaleno/designpatterns/adapter/data/RelogioDoSistema.kt
+++ src/main/kotlin/com/dmagdaleno/designpatterns/adapter/data/RelogioDoSistema.kt
@@ -3,7 +3,7 @@ package com.dmagdaleno.designpatterns.adapter.data
 import java.time.LocalDate
 
 class RelogioDoSistema : Relogio {
-    
+
     override fun hoje(): LocalDate {
         return LocalDate.now()
     }
